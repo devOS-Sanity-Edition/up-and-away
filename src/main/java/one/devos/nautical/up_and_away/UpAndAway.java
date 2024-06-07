7a821a12857d0b80d65eac4f4be6c154127f544f
TropheusJ <jverrellijr@icloud.com>
2024-06-06 20:29:42 -0400

    branding, more setup

--- src/main/java/one/devos/nautical/up_and_away/UpAndAway.java
+++ src/main/java/one/devos/nautical/up_and_away/UpAndAway.java
@@ -1,4 +1,4 @@
-package io.github.tropheusj.modid;
+package one.devos.nautical.up_and_away;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -7,8 +7,8 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String ID = "modid";
+public class UpAndAway implements ModInitializer {
+	public static final String ID = "up_and_away";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
