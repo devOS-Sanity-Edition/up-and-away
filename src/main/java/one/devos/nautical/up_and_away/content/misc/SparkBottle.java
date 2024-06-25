6a0edf47e5b4ba5190e88e7d5c308d4dd5abbc0c
maximumpower55 <maximumpowermc@gmail.com>
2024-06-25 15:59:05 -0700

    spark bottle sounds

--- src/main/java/one/devos/nautical/up_and_away/content/misc/SparkBottle.java
+++ src/main/java/one/devos/nautical/up_and_away/content/misc/SparkBottle.java
@@ -1,6 +1,8 @@
 package one.devos.nautical.up_and_away.content.misc;
 
 import net.minecraft.Optionull;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
@@ -16,11 +18,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
 
 import net.minecraft.world.phys.Vec3;
 import one.devos.nautical.up_and_away.content.UpAndAwayItems;
@@ -36,34 +35,31 @@ public class SparkBottle extends Item {
 
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level world, Player user, InteractionHand hand) {
-		BlockHitResult hit = getPlayerPOVHitResult(world, user, ClipContext.Fluid.ANY);
-		if (hit.getType() == HitResult.Type.MISS) {
-			if (!world.isClientSide) {
-				user.awardStat(Stats.ITEM_USED.get(this));
-
-				Vec3 start = user.getEyePosition();
-				Vec3 delta = user
-						.calculateViewVector(user.getXRot(), user.getYRot())
-						.scale(RANGE);
-				Entity target = Optionull.mapOrDefault(ProjectileUtil.getEntityHitResult(
-						user,
-						start,
-						start.add(delta),
-						user.getBoundingBox().expandTowards(delta).inflate(1),
-						EntitySelector.NO_SPECTATORS,
-						Mth.square(RANGE)
-				), EntityHitResult::getEntity, user);
-				world.addFreshEntity(new ShulkerBullet(world, user, target, null));
-			}
-
-			ItemStack stack = user.getItemInHand(hand);
-			return InteractionResultHolder.sidedSuccess(
-					!user.hasInfiniteMaterials() ?
-							ItemUtils.createFilledResult(user.getItemInHand(hand), user, Items.GLASS_BOTTLE.getDefaultInstance()) : stack,
-					world.isClientSide
-			);
+		if (!world.isClientSide) {
+			user.awardStat(Stats.ITEM_USED.get(this));
+			Vec3 start = user.getEyePosition();
+			Vec3 delta = user
+					.calculateViewVector(user.getXRot(), user.getYRot())
+					.scale(RANGE);
+			Entity target = Optionull.mapOrDefault(ProjectileUtil.getEntityHitResult(
+					user,
+					start,
+					start.add(delta),
+					user.getBoundingBox().expandTowards(delta).inflate(1),
+					EntitySelector.NO_SPECTATORS,
+					Mth.square(RANGE)
+			), EntityHitResult::getEntity, user);
+			world.addFreshEntity(new ShulkerBullet(world, user, target, null));
 		}
-		return super.use(world, user, hand);
+
+		user.playSound(SoundEvents.SHULKER_BULLET_HIT);
+
+		ItemStack stack = user.getItemInHand(hand);
+		return InteractionResultHolder.sidedSuccess(
+				!user.hasInfiniteMaterials() ?
+						ItemUtils.createFilledResult(user.getItemInHand(hand), user, Items.GLASS_BOTTLE.getDefaultInstance()) : stack,
+				world.isClientSide
+		);
 	}
 
 	public static InteractionResult onUseEntity(Player player, Level world, InteractionHand hand, Entity entity, @Nullable EntityHitResult hitResult) {
@@ -71,6 +67,8 @@ public class SparkBottle extends Item {
 		if (entity.getType().equals(EntityType.SHULKER_BULLET) && stack.is(Items.GLASS_BOTTLE)) {
 			if (!world.isClientSide) {
 				player.awardStat(Stats.ITEM_USED.get(Items.GLASS_BOTTLE));
+				// neutral sound source for consistency+ with dragon breath filling
+				world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.BOTTLE_FILL_DRAGONBREATH, SoundSource.NEUTRAL, 1f, 1f);
 				player.setItemInHand(hand, ItemUtils.createFilledResult(stack, player, UpAndAwayItems.SPARK_BOTTLE.getDefaultInstance()));
 				entity.kill();
 			}
